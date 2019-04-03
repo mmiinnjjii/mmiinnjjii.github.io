345ac46cf8b2847995d12b5952de1f5b1b216190
limminji <minji9873@naver.com>
2019-04-03 21:56:24 +0900

    no message

---
+++ js/common.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+    /* 메인 타이핑 */
+    consoleText(['Welcome', 'LIMMINJI Site', 'Nice to meet you'], 'text');
+    // ,['tomato','rebeccapurple','lightblue']
+    // 'text' 뒤에 붙이면 color 반영
+
+    function consoleText(words, id, colors) {
+      if (colors === undefined) colors = ['#fff'];
+      var visible = true;
+      var con = document.getElementById('console');
+      var letterCount = 1;
+      var x = 1;
+      var waiting = false;
+      var target = document.getElementById(id)
+      target.setAttribute('style', 'color:' + colors[0])
+      window.setInterval(function() {
+
+        if (letterCount === 0 && waiting === false) {
+          waiting = true;
+          target.innerHTML = words[0].substring(0, letterCount)
+          window.setTimeout(function() {
+            var usedColor = colors.shift();
+            colors.push(usedColor);
+            var usedWord = words.shift();
+            words.push(usedWord);
+            x = 1;
+            target.setAttribute('style', 'color:' + colors[0])
+            letterCount += x;
+            waiting = false;
+          }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+          waiting = true;
+          window.setTimeout(function() {
+            x = -1;
+            letterCount += x;
+            waiting = false;
+          }, 1000)  //문장 체인지 속도
+        } else if (waiting === false) {
+          target.innerHTML = words[0].substring(0, letterCount)
+          letterCount += x;
+        }
+      }, 150) //타이핑속도
+      window.setInterval(function() {
+        if (visible === true) {
+          con.className = 'underbar hidden'
+          visible = false;
+
+        } else {
+          con.className = 'underbar'
+
+          visible = true;
+        }
+      }, 400)
+    };
+    
+    /* 페이지 이동 */
+    $(".content").each(function () {
+        // 개별적으로 Wheel 이벤트 적용
+        $(this).on("mousewheel DOMMouseScroll", function (e) {
+            e.preventDefault();
+            var delta = 0;
+            if (!event) event = window.event;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                if (window.opera) delta = -delta;
+            } else if (event.detail) delta = -event.detail / 3;
+            var moveTop = null;
+            // 마우스휠을 위에서 아래로
+            if (delta < 0) {
+                if ($(this).next() != undefined) {
+                    moveTop = $(this).next().offset().top;
+                }
+            // 마우스휠을 아래에서 위로
+            } else {
+                if ($(this).prev() != undefined) {
+                    moveTop = $(this).prev().offset().top;
+                }
+            }
+            // 화면 이동 0.8초(800)
+            $("html,body").stop().animate({
+                scrollTop: moveTop + 'px'
+            }, {
+                duration: 800, complete: function () {
+                }
+            });
+        });
+    });
+});
